7ba55a13633d3e49274130da59a3a92b6a898c32
Unknown <thomasray0@gmail.com>
2017-10-20 18:18:28 -0400

    v1.2.3-alpha

--- slim-skeleton/classes/ContentUpdater.php
+++ slim-skeleton/classes/ContentUpdater.php
@@ -465,16 +465,18 @@ class ContentUpdater
             $result = "Success";
         }
         $sql = "CREATE TABLE `pages` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `name` varchar(64) NOT NULL,
-            `page-title` varchar(32) NOT NULL,
-            `author` varchar(32) NOT NULL,
-            `is-fullwidth` tinyint(1) NOT NULL DEFAULT '0',
-            `content` text NOT NULL,
-            `last-modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (`id`),
-            UNIQUE KEY `name` (`name`(32))
-          ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `page-title` varchar(32) NOT NULL,
+  `author` varchar(32) NOT NULL,
+  `is-fullwidth` tinyint(1) NOT NULL DEFAULT '0',
+  `content` text NOT NULL,
+  `template` varchar(45) NOT NULL DEFAULT 'content.phtml',
+  `last-modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`(32))
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
           ";
         if (!mysqli_query($conn, $sql)) {
             $result = mysqli_error($conn);
--- slim-skeleton/templates/page.php
+++ slim-skeleton/templates/page.php
@@ -2,7 +2,16 @@
 require '../config.php';
 
 if ($name !== 'blog' && $name !== 'blog-content') {
-  include_once "themes/{$theme}/content.phtml";
+  $sql = "SELECT * FROM `afroraydude-site`.`pages` WHERE `name` LIKE '{$name}'";
+
+  $stmt = $conn->query($sql);
+  $template = 'content.phtml';
+  if (0 !== $stmt->rowCount()) {
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $template = $row['template'];
+    }
+  }
+  include_once "themes/{$theme}/{$template}";
 } else if ($name !== 'blog-content') {
   include_once "themes/{$theme}/blog.phtml";
 } else {
